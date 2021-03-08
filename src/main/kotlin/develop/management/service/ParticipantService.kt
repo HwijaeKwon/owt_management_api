c414e712a296cabc380103a65282aef758511fd0
hwijae_kwon <hwijae_kwon@tmax.co.kr>
2021-03-09 03:57:55 +0900

    [feat] Participant Rpc 구현
    - Participant Rpc 구현

--- src/main/kotlin/develop/management/service/ParticipantService.kt
+++ src/main/kotlin/develop/management/service/ParticipantService.kt
@@ -1,52 +1,81 @@
 package develop.management.service
 
+import com.google.gson.Gson
 import develop.management.domain.dto.ParticipantDetail
 import develop.management.domain.dto.Permission
 import develop.management.domain.dto.PermissionUpdate
+import develop.management.domain.dto.StreamInfo
+import develop.management.rpc.RpcService
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import org.springframework.stereotype.Service
 
 /**
  * 사용자 관련 비즈니스 로직을 담당하는 service class
  */
 @Service
-class ParticipantService {
+class ParticipantService(private val rpcService: RpcService) {
 
     /**
-     * ManagementAPI
      * Conference에서 사용자를 조회한다
      */
-    suspend fun findOne(roomId: String, participantId: String): ParticipantDetail? {
-        //Todo: rpc를 통해 participant 정보를 가져와야 한다
-        // Rpc error시 exception을 던져야 한다
-        return ParticipantDetail("id", "role", "user", Permission(Permission.Publish(audio = true, video = true), Permission.Subscribe(audio = true, video = true)))
+    suspend fun findOne(roomId: String, participantId: String): ParticipantDetail {
+        val (status, result) = rpcService.getParticipantsInRoom(roomId)
+        if(status == "error") throw IllegalStateException("Get participants in room fail. $result")
+
+        val jsonArray = JSONArray()
+        val participantArray = mutableListOf<JSONObject>()
+        try {
+            var i = 0
+            while (true) {
+                participantArray.add(jsonArray.getJSONObject(i))
+                i++
+            }
+        } catch (e: JSONException) {
+            //
+        }
+        return participantArray.firstOrNull { it.getString("id") == participantId }?.let {
+            Gson().fromJson(it.toString(), ParticipantDetail::class.java)
+        }?: throw IllegalArgumentException("Participant not found")
     }
 
     /**
-     * ManagementAPI
      * Conference에서 특정 방의 모든 사용자를 조회한다
      */
     suspend fun findAll(roomId: String): List<ParticipantDetail> {
-        //Todo: rpc를 통해 participant 정보를 가져와야 한다
-        // Rpc error시 exception을 던져야 한다
-        return listOf(ParticipantDetail("id", "role", "user", Permission(Permission.Publish(audio = true, video = true), Permission.Subscribe(audio = true, video = true))))
+        val (status, result) = rpcService.getParticipantsInRoom(roomId)
+        if(status == "error") throw IllegalStateException("Get participants in room fail. $result")
+
+        val jsonArray = JSONArray()
+        val participantArray = mutableListOf<JSONObject>()
+        try {
+            var i = 0
+            while (true) {
+                participantArray.add(jsonArray.getJSONObject(i))
+                i++
+            }
+        } catch (e: JSONException) {
+            //
+        }
+        return participantArray.map { jsonObject -> Gson().fromJson(jsonObject.toString(), ParticipantDetail::class.java) }
     }
 
     /**
-     * ManagementAPI
      * 특정 room의 사용자 권한을 conference에 업데이트 한다
      */
-    suspend fun update(roomId: String, participantId: String, permissionUpdate: PermissionUpdate): ParticipantDetail? {
-        //Todo: rpc를 통해 participant 정보를 업데이트해야 한다
-        // Rpc error시 exception을 던져야 한다
-        return ParticipantDetail("id", "role", "user", Permission(Permission.Publish(audio = true, video = true), Permission.Subscribe(audio = true, video = true)))
+    suspend fun update(roomId: String, participantId: String, permissionUpdate: PermissionUpdate): ParticipantDetail {
+        val (status, result) = rpcService.updateParticipant(roomId, participantId, permissionUpdate)
+        if(status == "error") throw IllegalStateException("Update participant fail. $result")
+
+        return Gson().fromJson(result, ParticipantDetail::class.java)
     }
 
     /**
-     * ManagementAPI
      * Conference에서 특정 room의 특정 participant를 제거한다
      */
     suspend fun delete(roomId: String, participantId: String) {
-        //Todo: rpc를 통해 participant 정보를 삭제해야 한다
-        // Rpc error시 exception을 던져야 한다
+        val (status, result) = rpcService.deleteParticipant(roomId, participantId)
+        if(status == "error") throw IllegalStateException("Delete participant fail. $result")
     }
 }
